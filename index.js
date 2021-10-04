c89cb2bd6b3a6a74633b12fd8e0bd7e1d302a8a6
Raman Shalupau <raman.shalupau@gmail.com>
2021-10-04 17:14:04 +0800

    1.0.15

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-require('fs')
-  .readdirSync('./locales/')
-  .forEach(file => {
-    if (file.match(/\.js$/) !== null && file !== 'index.js') {
-      let name = file.replace('.js', '')
-      module.exports[name] = require('./locales/' + file)
-    }
-  })
+// This will be executed in the browser, compiled by webpack, hence no `fs` please.
+module.exports = {
+  en: require('./locales/en'),
+  ru: require('./locales/ru'),
+  de: require('./locales/de'),
+  pt: require('./locales/pt'),
+  es: require('./locales/es'),
+  // Add your locale import here
+}
