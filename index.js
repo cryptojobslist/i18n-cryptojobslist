577e73409a632d44c69daa572899c07a34588a94
Raman Shalupau <raman.shalupau@gmail.com>
2021-10-04 17:13:37 +0800

    generalized export of locales

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-module.exports = {
-  en: require('./locales/en'),
-  ru: require('./locales/ru'),
-  de: require('./locales/de'),
-  pt: require('./locales/pt'),
-}
+require('fs')
+  .readdirSync('./locales/')
+  .forEach(file => {
+    if (file.match(/\.js$/) !== null && file !== 'index.js') {
+      let name = file.replace('.js', '')
+      module.exports[name] = require('./locales/' + file)
+    }
+  })
